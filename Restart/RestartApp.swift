e80a1f43f7264fce1360720ba1e65a69d9323d99
Dzmitry Bladyka <dzmitrydistortion@gmail.com>
2023-03-26 23:51:46 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Dzmitry Bladyka on 26.03.23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
