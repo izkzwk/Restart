6b7e4f42a7cdf51666a643fef718cb5880197908
Dzmitry Bladyka <dzmitrydistortion@gmail.com>
2023-03-27 02:50:25 +0300

    Completed project

---
+++ Restart/Utilities/AudioPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  AudioPlayer.swift
+//  Restart
+//
+//  Created by Dzmitry Bladyka on 27.03.23.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not play the sound file.")
+        }
+    }
+}
